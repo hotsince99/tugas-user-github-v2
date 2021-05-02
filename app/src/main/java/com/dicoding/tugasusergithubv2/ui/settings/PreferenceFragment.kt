cdb485c98633b3f037dd8fc0c3a4b4886e83d365
Josua Margandatua Hutapea <65341897+hotsince99@users.noreply.github.com>
2021-05-02 17:31:59 +0700

    added alarm notification to open the app at 09:00AM

--- app/src/main/java/com/dicoding/tugasusergithubv2/ui/settings/PreferenceFragment.kt
+++ app/src/main/java/com/dicoding/tugasusergithubv2/ui/settings/PreferenceFragment.kt
@@ -8,6 +8,7 @@ import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SwitchPreference
 import com.dicoding.tugasusergithubv2.R
+import com.dicoding.tugasusergithubv2.alarm.AlarmReceiver
 
 
 class PreferenceFragment : PreferenceFragmentCompat(),
@@ -17,6 +18,7 @@ class PreferenceFragment : PreferenceFragmentCompat(),
     private lateinit var LANGUAGE: String
     private lateinit var isAlarmActivated: SwitchPreference
     private lateinit var changeLanguage: Preference
+    private lateinit var alarmReceiver: AlarmReceiver
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         addPreferencesFromResource(R.xml.fragment_preference)
@@ -24,18 +26,30 @@ class PreferenceFragment : PreferenceFragmentCompat(),
         init()
         setSummaries()
     }
-
     private fun setSummaries() {
         val sharedPreference = preferenceManager.sharedPreferences
         isAlarmActivated.isChecked = sharedPreference.getBoolean(ALARM, false)
     }
-
     private fun init() {
         ALARM = resources.getString(R.string.key_alarm)
         LANGUAGE = resources.getString(R.string.key_language)
         isAlarmActivated = findPreference<SwitchPreference>(ALARM) as SwitchPreference
         changeLanguage = findPreference<Preference>(LANGUAGE) as Preference
 
+        alarmReceiver = AlarmReceiver()
+
+        isAlarmActivated.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _, newValue ->
+            val isChecked = newValue as? Boolean ?: false
+
+            if (isChecked) {
+                // set alarm
+                alarmReceiver.setRepeatingAlarm(requireContext())
+            } else {
+                // cancel alarm
+                alarmReceiver.cancelAlarm(requireContext())
+            }
+            true
+        }
 
         changeLanguage.onPreferenceClickListener = Preference.OnPreferenceClickListener {
             val intent = Intent(Settings.ACTION_LOCALE_SETTINGS)
@@ -52,7 +66,6 @@ class PreferenceFragment : PreferenceFragmentCompat(),
         super.onPause()
         preferenceScreen.sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
     }
-
     override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
         if (key == ALARM) {
             isAlarmActivated.isChecked = sharedPreferences.getBoolean(ALARM, false)
