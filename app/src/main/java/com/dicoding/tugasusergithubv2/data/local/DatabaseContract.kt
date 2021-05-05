44e3e0bafc68c4985216aeea40284e9ce7fd3480
Josua Margandatua Hutapea <65341897+hotsince99@users.noreply.github.com>
2021-05-05 20:58:36 +0700

    define Content URI

--- app/src/main/java/com/dicoding/tugasusergithubv2/data/local/DatabaseContract.kt
+++ app/src/main/java/com/dicoding/tugasusergithubv2/data/local/DatabaseContract.kt
@@ -1,14 +1,25 @@
 package com.dicoding.tugasusergithubv2.data.local
 
+import android.net.Uri
 import android.provider.BaseColumns
 
 internal class DatabaseContract {
+
     internal class FavoriteColumns : BaseColumns {
         companion object {
             const val TABLE_NAME = "favorite_users"
             const val _ID = "_id"
             const val LOGIN = "login"
             const val AVATAR_URL = "avatar_url"
+
+            const val AUTHORITY = "com.dicoding.tugasusergithubv2"
+            const val SCHEME = "content"
+
+            val CONTENT_URI: Uri = Uri.Builder()
+                .scheme(SCHEME)
+                .authority(AUTHORITY)
+                .appendPath(TABLE_NAME)
+                .build()
         }
     }
 }
